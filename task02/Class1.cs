5688ae1097c8ca3a58c7627ea451ffa723cc95d5
kisswakk <ivan.shcherbak.07@gmail.com>
2026-06-25 14:10:18 +0600

    Fix a warnings with null for class srudent by setting basic values

--- task02/Class1.cs
+++ task02/Class1.cs
@@ -2,9 +2,9 @@
 
 public class Student
 {
-    public string Name { get; set; }
-    public string Faculty { get; set; }
-    public List<int> Grades { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Faculty { get; set; } = string.Empty;
+    public List<int> Grades { get; set; } = new List<int>();
 }
 public class StudentService
 {
