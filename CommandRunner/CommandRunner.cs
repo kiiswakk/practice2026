f6b8dc4542adb51662287c02f39996b058ed88b9
kisswakk <ivan.shcherbak.07@gmail.com>
2026-07-01 16:28:29 +0600

    Create task08

---
+++ CommandRunner/CommandRunner.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+
+var dllPath = Path.Combine(
+    AppDomain.CurrentDomain.BaseDirectory,
+    "FileSystemCommands.dll");
+
+if (!File.Exists(dllPath))
+{
+    Console.WriteLine("Не удалось найти файл");
+    return;
+}
+
+Assembly assembly = Assembly.LoadFrom(dllPath);
+
+var commandTypes = assembly.GetTypes()
+    .Where(type =>
+        typeof(ICommand).IsAssignableFrom(type) &&
+        !type.IsInterface &&
+        !type.IsAbstract);
+
+foreach (var type in commandTypes)
+{
+    ICommand? command = null;
+
+    if (type.Name == "DirectorySizeCommand")
+    {
+        command = (ICommand?)Activator.CreateInstance(type, ".");
+    }
+    else if (type.Name == "FindFilesCommand")
+    {
+        command = (ICommand?)Activator.CreateInstance(type, ".", "*.cs");
+    }
+
+    command?.Execute();
+}
