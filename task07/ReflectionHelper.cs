c3e19aad956882ba51ab3722f185f609f119530a
kisswakk <ivan.shcherbak.07@gmail.com>
2026-07-06 15:52:59 +0600

    Fix names

--- task07/ReflectionHelper.cs
+++ task07/ReflectionHelper.cs
@@ -1,41 +1,6 @@
 ﻿namespace task07;
 using System.Reflection;
 
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property)]
-public class DisplayNameAttribute : Attribute
-{
-    public string DisplayName { get; }
-
-    public DisplayNameAttribute(string displayName)
-    {
-        DisplayName = displayName;
-    }
-}
-
-[AttributeUsage(AttributeTargets.Class)]
-public class VersionAttribute : Attribute
-{
-    public int Major { get; }
-    public int Minor { get; }
-
-    public VersionAttribute(int major, int minor)
-    {
-        Major = major;
-        Minor = minor;
-    }
-}
-
-[DisplayName("Пример класса")]
-[Version(1, 0)]
-public class SampleClass
-{   
-    [DisplayName("Числовое свойство")]
-    public int Number { get; set; }
-
-    [DisplayName("Тестовый метод")]
-    public void TestMethod() {}
-}
-
 public static class ReflectionHelper
 {
     public static void PrintTypeInfo(Type type)
