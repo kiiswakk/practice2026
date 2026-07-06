3d0113234e24d22a546961d022d68a157b662bbf
kisswakk <ivan.shcherbak.07@gmail.com>
2026-07-07 00:28:43 +0600

    Big fix to task09

--- task09/Program.cs
+++ task09/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 if (args.Length == 0)
 {
@@ -14,38 +17,74 @@ if (!File.Exists(dllPath))
     return;
 }
 
-Assembly assembly = Assembly.LoadFrom(dllPath);
-
-Console.WriteLine($"Библиотека: {assembly.GetName().Name}");
-
-foreach (Type type in assembly.GetTypes())
+try
 {
-    Console.WriteLine($"Класс: {type.Name}");
-
-    Console.WriteLine("Атрибуты:");
-    foreach (var attribute in type.GetCustomAttributes())
+    Assembly assembly = Assembly.LoadFrom(dllPath);
+    Console.WriteLine($"Библиотека: {assembly.GetName().Name}");
+    Console.WriteLine(new string('-', 40));
+    Type[] types;
+    try
     {
-        Console.WriteLine($"{attribute.GetType().Name}");
+        types = assembly.GetTypes();
     }
-
-    Console.WriteLine("Конструкторы:");
-    foreach (ConstructorInfo constructor in type.GetConstructors())
+    catch (ReflectionTypeLoadException ex)
     {
-        Console.WriteLine($"{constructor.Name}");
-
-        foreach (ParameterInfo parameter in constructor.GetParameters())
+        Console.WriteLine("Не удалось загрузить некоторые типы из-за отсутствующих зависимостей.");
+        
+        Console.WriteLine("Ошибки загрузки:");
+        foreach (var loaderException in ex.LoaderExceptions)
         {
-            Console.WriteLine($"{parameter.ParameterType.Name} {parameter.Name}");
+            Console.WriteLine($" - {loaderException?.Message}");
         }
+        Console.WriteLine(new string('-', 40));
+        types = ex.Types.OfType<Type>().ToArray();
     }
 
-    Console.WriteLine("Методы:");
-    foreach (MethodInfo method in type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+    foreach (Type type in types)
     {
-        Console.WriteLine($"{method.Name}");
-        foreach (ParameterInfo parameter in method.GetParameters())
+        Console.WriteLine($"Класс: {type.FullName}");
+
+        // Атрибуты
+        var attributes = type.GetCustomAttributes().ToArray();
+        if (attributes.Length > 0)
         {
-            Console.WriteLine($"{parameter.ParameterType.Name} {parameter.Name}");
+            Console.WriteLine("  Атрибуты:");
+            foreach (var attribute in attributes)
+            {
+                Console.WriteLine($"    [{attribute.GetType().Name}]");
+            }
         }
+        var constructors = type.GetConstructors();
+        if (constructors.Length > 0)
+        {
+            Console.WriteLine("  Конструкторы:");
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                var parameters = constructor.GetParameters()
+                    .Select(p => $"{p.ParameterType.Name} {p.Name}");
+                Console.WriteLine($"    ctor({string.Join(", ", parameters)})");
+            }
+        }
+        var methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+        if (methods.Length > 0)
+        {
+            Console.WriteLine("  Методы:");
+            foreach (MethodInfo method in methods)
+            {
+                var parameters = method.GetParameters()
+                    .Select(p => $"{p.ParameterType.Name} {p.Name}");
+                Console.WriteLine($"    {method.ReturnType.Name} {method.Name}({string.Join(", ", parameters)})");
+            }
+        }
+        
+        Console.WriteLine();
     }
+}
+catch (BadImageFormatException)
+{
+    Console.WriteLine("Ошибка: Файл не является валидной .NET сборкой.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ошибка: {ex.Message}");
 }
