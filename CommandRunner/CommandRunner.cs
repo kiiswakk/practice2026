c6023a22df5b2c15d71970fd5227f7ef5df98fca
kisswakk <ivan.shcherbak.07@gmail.com>
2026-07-06 18:05:37 +0600

    Fix name and add try, catch

--- CommandRunner/CommandRunner.cs
+++ CommandRunner/CommandRunner.cs
@@ -11,26 +11,52 @@ if (!File.Exists(dllPath))
     return;
 }
 
-Assembly assembly = Assembly.LoadFrom(dllPath);
-
-var commandTypes = assembly.GetTypes()
-    .Where(type =>
-        typeof(ICommand).IsAssignableFrom(type) &&
-        !type.IsInterface &&
-        !type.IsAbstract);
-
-foreach (var type in commandTypes)
+try
 {
-    ICommand? command = null;
+    Assembly assembly = Assembly.LoadFrom(dllPath);
 
-    if (type.Name == "DirectorySizeCommand")
-    {
-        command = (ICommand?)Activator.CreateInstance(type, ".");
-    }
-    else if (type.Name == "FindFilesCommand")
-    {
-        command = (ICommand?)Activator.CreateInstance(type, ".", "*.cs");
-    }
+    var commandTypes = assembly.GetTypes()
+        .Where(type =>
+            typeof(ICommand).IsAssignableFrom(type) &&
+            !type.IsInterface &&
+            !type.IsAbstract);
 
-    command?.Execute();
+    foreach (var type in commandTypes)
+    {
+        try
+        {
+            ICommand? command = null;
+
+            if (type.Name == "DirectorySizeCommand")
+            {
+                command = (ICommand?)Activator.CreateInstance(type, ".");
+            }
+            else if (type.Name == "FindFilesCommand")
+            {
+                command = (ICommand?)Activator.CreateInstance(type, ".", "*.cs");
+            }
+
+            if (command != null)
+            {
+                command.Execute();
+            }            
+        }
+        catch (MissingMethodException)
+        {
+            Console.WriteLine($"[Ошибка] У типа {type.Name} не найден подходящий конструктор.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"[Ошибка] Не удалось инициализировать или выполнить команду {type.Name}: {ex.Message}");
+        }
+    }
+}
+
+catch (FileLoadException ex)
+{
+    Console.WriteLine($"[Критическая ошибка] Не удалось загрузить сборку: {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"[Непредвиденная ошибка]: {ex.Message}");
 }
